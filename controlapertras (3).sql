2bf97e06970b0292f9c19aa75ab3c0ccd01b1027
Gabriel <jehernandezh@gcch.com>
2024-05-24 14:00:18 -0600

    actualizacion de base de datos

--- controlapertras (3).sql
+++ controlapertras (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2024 a las 20:52:32
+-- Tiempo de generación: 24-05-2024 a las 21:59:21
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `controlapertras`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamentos`
+--
+
+CREATE TABLE `departamentos` (
+  `id_departamento` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamentos`
+--
+
+INSERT INTO `departamentos` (`id_departamento`, `Nombre`) VALUES
+(1, 'Direccion TI'),
+(2, 'Ventas'),
+(3, 'Inmobiliaria'),
+(4, 'RR.HH.'),
+(5, 'Cobranza');
 
 -- --------------------------------------------------------
 
@@ -43,16 +65,17 @@ INSERT INTO `pasosconsecutivos` (`Id_agregar`, `Nom_apertura`, `Departamento_res
 (198, 'Alta de controladores', 'Direccion TI', 'Juan', 1),
 (199, 'Alta de promociones', 'Ventas', 'Enrique', 2),
 (200, 'Alta de precios', 'Ventas', 'Fernando', 4),
-(201, 'Envio de bines', 'Cobranza', 'Juan', 5),
-(202, 'Envio de precios', 'Ventas', 'Fernando', 6),
-(203, 'Ala auditoria', 'Cobranza', 'Enrique', 7),
-(204, 'Alta autorizadores bancarios', 'Cobranza', 'Fernando', 8),
-(205, 'Alta Q-promo', 'Inmobiliaria', 'Pedro', 9),
-(206, 'Alta ticket digital', 'Direccion TI', 'Miguel Angel', 10),
-(207, 'Alta produtos', 'Inmobiliaria', 'Enrique', 11),
-(208, 'Pruebas integrales', 'Direccion TI', 'Gabriel', 12),
-(209, 'Apertura', 'Inmobiliaria', 'Fernando', 13),
-(210, 'prueba de paso 1', 'Direccion TI', 'Gabriel', 3);
+(201, 'Envio de bines', 'Cobranza', 'Juan', 6),
+(202, 'Envio de precios', 'Ventas', 'Fernando', 7),
+(203, 'Ala auditoria', 'Cobranza', 'Enrique', 8),
+(204, 'Alta autorizadores bancarios', 'Cobranza', 'Fernando', 9),
+(205, 'Alta Q-promo', 'Inmobiliaria', 'Pedro', 10),
+(206, 'Alta ticket digital', 'Direccion TI', 'Miguel Angel', 11),
+(207, 'Alta produtos', 'Inmobiliaria', 'Enrique', 12),
+(208, 'Pruebas integrales', 'Direccion TI', 'Gabriel', 13),
+(209, 'Apertura', 'Inmobiliaria', 'Fernando', 14),
+(210, 'prueba de paso 1', 'Direccion TI', 'Gabriel', 3),
+(211, 'prueba de paso 2', 'Ventas', 'Pedro', 5);
 
 -- --------------------------------------------------------
 
@@ -74,20 +97,11 @@ CREATE TABLE `tiendas` (
 --
 
 INSERT INTO `tiendas` (`id`, `Nom_tienda`, `N_tienda`, `Fecha_prueba`, `Fecha_apertura`, `Pasos_finalizados`) VALUES
-(71, 'SUPERCITO COL DOCTORES CUAUHTE', 206, '2022-07-30', '2022-08-12', '[1,1,1,1,1,1,1,1,1,1,0,0,0]'),
-(72, 'SUPERCITO XALAPA REVOLUCION', 207, '2023-09-13', '2023-10-16', '[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]'),
-(73, 'SUPERCITO COL DOCTORES CUAUHTE', 209, '2023-02-14', '2023-05-21', '[1,1,1,0,0,0,0,0,0,0,0,0,0,0]'),
-(74, 'SUPERCITO XALAPA CLAVIJERO', 211, '2024-04-09', '2024-05-09', '[1,1,1,1,1,1,1,1,1,1,1,1,0,0]'),
-(75, 'SUPERCITO XALAPA REVOLUCION', 212, '2023-02-14', '2023-04-19', '[1,1,1,1,1,1,1,1,1,1,1,1,0,0]'),
-(76, 'SUPERCITO COL DOCTORES CUAUHTE', 213, '2023-05-02', '2023-06-23', '[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(78, 'SUPERCITO XALAPA CLAVIJERO', 214, '2023-07-13', '2023-08-25', '[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(79, 'SUPERCITO XALAPA DOS CAMINOS', 215, '2023-06-14', '2023-07-19', '[1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(91, 'SUPERCITO LOPEZ MATEOS ATIZAPA', 225, '2024-04-11', '2024-05-03', '[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(92, 'SUPERCITO COL DOCTORES CUAUHTE', 227, '2024-04-02', '2024-05-01', '[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(105, 'SUPERCITO VERACRUZ DOS CAMINOS', 228, '2024-05-14', '2024-06-03', '[1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(107, 'SUPERCITO VERACRUZ CLAVIJERO', 230, '2024-05-08', '2024-05-30', '[1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]'),
-(109, 'SUPERCITO XALAPA REVOLUCION', 231, '2022-07-13', '2022-08-25', '[1,1,1,1,1,1,1,1,0,0,0,0,0,0]'),
-(110, 'SUPERCITO LOPEZ MATEOS ATIZAPA', 895, '2024-04-08', '2024-04-22', '[]');
+(118, 'SUPERCITO VERACRUZ CLAVIJERO', 910, '2024-04-30', '2024-05-16', '[1,1,1,1,1,0,0,0,0,0,0,0,0,0]'),
+(121, 'SUPERCITO LOPEZ MATEOS ATIZAPA', 891, '2024-01-10', '2024-02-14', '[]'),
+(122, 'SUPERCITO VERACRUZ DOS CAMINOS', 219, '2024-03-12', '2024-04-15', '[]'),
+(123, 'SUPERCITO XALAPA REVOLUCION', 893, '2024-06-04', '2024-07-09', '[]'),
+(124, 'SUPERCITO XALAPA CLAVIJERO', 206, '2024-05-09', '2024-05-15', '[]');
 
 -- --------------------------------------------------------
 
@@ -115,12 +129,17 @@ INSERT INTO `usuarios` (`id`, `Nombre`, `Apellidos`, `Correo_electrónico`, `N_e
 (2, 'Gabriel', 'Campos', 'Gabo@chedraui.com', 1045, 'Usuario', 'Direccion TI', '987654321'),
 (6, 'Juan', 'Marquez', 'Jmaz@chedraui.com', 1027, 'Usuario', 'Direccion TI', '12345676543'),
 (7, 'Pedro', 'Martinez', 'PeMa@chedraui.com', 1028, 'Usuario', 'Ventas', '9876513245'),
-(9, 'Fernando', 'Hernandez', 'Fher@chedraui.com', 1429, 'Usuario', 'Cobranza', '13216565e1fw1ef2'),
-(10, 'Enrique', 'Fernandez', 'Efer@chedraui.com', 1025, 'Usuario', 'Inmobiliaria', '132165655');
+(9, 'Fernando', 'Hernandez', 'Fher@chedraui.com', 1429, 'Usuario', 'Cobranza', '13216565e1fw1e');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  ADD PRIMARY KEY (`id_departamento`);
 
 --
 -- Indices de la tabla `pasosconsecutivos`
@@ -145,16 +164,22 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `departamentos`
+--
+ALTER TABLE `departamentos`
+  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `pasosconsecutivos`
 --
 ALTER TABLE `pasosconsecutivos`
-  MODIFY `Id_agregar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=211;
+  MODIFY `Id_agregar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=212;
 
 --
 -- AUTO_INCREMENT de la tabla `tiendas`
 --
 ALTER TABLE `tiendas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
