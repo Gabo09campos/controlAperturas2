24697b32582be97bc923e181edb2be2374ad1642
Gabriel <jehernandezh@gcch.com>
2024-06-04 13:14:05 -0600

    correccion a funciones de usuarios en pasos

--- cliente/js/pasos.js
+++ cliente/js/pasos.js
@@ -3,8 +3,6 @@ let pasos = document.getElementById("pasosApertura");
 let estadoBotones = []; // Creamos un objeto para almacenar los botones que ya fueron completados.
 // Al cargar la página, obtenemos los pasos guardados en localStorage y parseamos el String un Array.
 let pasosGuardados = JSON.parse(localStorage.getItem('PTAU'));
-let depaUsuario = localStorage.getItem('DUA'); // Traemos el departamento del usuario que inicio sesion (como string).
-let permisos = localStorage.getItem('PUU'); // Traemos los valores de si tiene o no permisos el usuario.
 /*************************************************************************************
  * Con un fetch indicamos cual sera la url en donde nos mostrara los datos en la web.
  * Con .then recibimos la respuesta de la base de datos y con el json lo interpreta a una manera legible para el usuario.
@@ -40,93 +38,61 @@ fetch("http://localhost:3004/pasos")
         
         pasos.appendChild(row);
         /*********************************************************** */
-        // En una variable ponemos los departamentos de los pasos.
-        let depResponsable = apertura.Departamento_responsble;
         // En el nombre de cada boton ponemos un evento de escucha para el click.
         Nom_apertura.addEventListener('click', function(e) {
             console.log(permisos);
             // Prevenimos que la pagina se recargue.
             e.preventDefault();
-            if(permisos === 0 || permisos === "0" || permisos === false){
-                // Si el departamento del usuario es diferente al del paso, se deshabilita el boton.
-                const btnPaso = document.getElementsByTagName('button')
-                btnPaso.disabled = true; 
-                // Enviamos un mensaje de error en caso de dar click.
+            // En una variable llamamos los pasos.
+            let numPaso = apertura.Num_paso;
+            // Con un condicional verificamos que no se pueda marcar un paso hasta que el anterior este completado.
+            if (index === 0 || estadoBotones[rest[index - 1].Num_paso] === 1) {
+                estadoBotones[numPaso] = 1; // Marcar como finalizado.
+                // Al estar finalizado el paso, cambia su color.
+                Nom_apertura.style.color = "white";
+                Nom_apertura.style.backgroundColor = "green";
+                // Traemos el ID de la tienda en la que estamos (localStorage).
+                let idTiendaActual = localStorage.getItem('TAU');
+                    // Enviamos los datos del objeto a la tienda correspondiente.
+                    // Esta comentado para las pruebas.
+                    if(idTiendaActual){
+                        // Filtramos y eliminamos todos los valores que no sean booleano.
+                        let nuevoEstado = estadoBotones.filter(value => value !== null && value !== "null" && value !== undefined && value !== "" && (value === 0 || value === 1));
+                        // Enviar el estado de los botones a la base de datos.
+                        fetch('http://localhost:3004/agregarPasoFinzalizado', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                id: idTiendaActual,
+                                Id_agregar: numPaso,
+                                estado: nuevoEstado // Enviamos todo el objeto.
+                            }),
+                        })
+                        .then(response => response.json())
+                        .then(data => console.log('Success:', data))
+                        .catch((error) => console.error('Error:', error)); 
+                    }
+                    // Si ya finalizo en paso, se le reedirige hacia el index y se guarda el valor del boton como finalizado.
+                    Swal.fire({
+                        icon: "success",
+                        title: "Buen trabajo equipo",
+                        showConfirmButton: false,
+                        timer: 1500
+                    }).then((result) => {
+                        // Redirige solo después de que se cierre la alerta.
+                        location.href = 'index.html';
+                    });
+            } else {
+                // Si no se cumple el condicional de estar finalizado el paso anterior enviamos un mensaje.
                 Swal.fire({
                     icon: "error",
                     title: 'Error!',
-                    text: 'No tienes los permisos necesarios',
+                    text: 'Debes completar el paso anterior',
                     confirmButtonText: 'Continuar',
                 })
-            }else{
-                // Comparamos si el departamento del usuario (localStorage) coincide con el del paso.
-                if (depaUsuario === depResponsable) {
-                    console.log('Los arreglos son equivalentes.'); 
-                    // En una variable llamamos los pasos.
-                    let numPaso = apertura.Num_paso;
-                    // Con un condicional verificamos que no se pueda marcar un paso hasta que el anterior este completado.
-                    if (index === 0 || estadoBotones[rest[index - 1].Num_paso] === 1) {
-                        estadoBotones[numPaso] = 1; // Marcar como finalizado.
-                        // Al estar finalizado el paso, cambia su color.
-                        Nom_apertura.style.color = "white";
-                        Nom_apertura.style.backgroundColor = "green";
-                        // Traemos el ID de la tienda en la que estamos (localStorage).
-                        let idTiendaActual = localStorage.getItem('TAU');
-                            // Enviamos los datos del objeto a la tienda correspondiente.
-                            // Esta comentado para las pruebas.
-                        /* if(idTiendaActual){
-                                // Filtramos y eliminamos todos los valores que no sean booleano.
-                                let nuevoEstado = estadoBotones.filter(value => value !== null && value !== "null" && value !== undefined && value !== "" && (value === 0 || value === 1));
-                                // Enviar el estado de los botones a la base de datos.
-                                fetch('http://localhost:3004/agregarPasoFinzalizado', {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                    },
-                                    body: JSON.stringify({
-                                        id: idTiendaActual,
-                                        Id_agregar: numPaso,
-                                        estado: nuevoEstado // Enviamos todo el objeto.
-                                    }),
-                                })
-                                .then(response => response.json())
-                                .then(data => console.log('Success:', data))
-                                .catch((error) => console.error('Error:', error)); 
-                            }*/
-                            // Si ya finalizo en paso, se le reedirige hacia el index y se guarda el valor del boton como finalizado.
-                            Swal.fire({
-                                icon: "success",
-                                title: "Buen trabajo equipo",
-                                showConfirmButton: false,
-                                timer: 1500
-                            }).then((result) => {
-                                // Redirige solo después de que se cierre la alerta.
-                                location.href = 'index.html';
-                            });
-                    } else {
-                        // Si no se cumple el condicional de estar finalizado el paso anterior enviamos un mensaje.
-                        Swal.fire({
-                            icon: "error",
-                            title: 'Error!',
-                            text: 'Debes completar el paso anterior',
-                            confirmButtonText: 'Continuar',
-                        })
-                    } 
-                } else {
-                    console.log('Los arreglos son diferentes.');
-                    // Si el departamento del usuario es diferente al del paso, se deshabilita el boton.
-                    const btnPaso = document.getElementsByTagName('button')
-                    btnPaso.disabled = true; 
-                    // Enviamos un mensaje de error en caso de dar click.
-                    Swal.fire({
-                        icon: "error",
-                        title: 'Error!',
-                        text: 'No tienes acceso a este boton',
-                        confirmButtonText: 'Continuar',
-                    })
-                }
-            }
-            
+            }     
         });
         /************************************************* */
         // Funcion para buscar pasos por su nombre.
