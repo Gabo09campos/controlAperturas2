48deb37dd23eaa25509f6a19ff13f1184a27f1bd
Gabriel <jehernandezh@gcch.com>
2024-07-18 10:47:39 -0600

    cambio a conexion de base de datos en linea

--- conexion.js
+++ conexion.js
@@ -6,10 +6,10 @@ const baseDatos = require("mysql");
     (host=nombre de base de datos, database=nombre de la base de datos, user=nombre de usuario, password).
 */
 const conexion = baseDatos.createConnection({
-    host: "localhost",
-    database: "controlapertras",
-    user: "root",
-    password: ""
+    host: "us-cluster-east-01.k8s.cleardb.net",
+    database: "heroku_e9ec0d785746c90",
+    user: "b74b53a5e76437",
+    password: "687f82c3"
 });
 /**
  * exportamos la conexion para que pueda ser requerida en otras hojas.
@@ -20,14 +20,14 @@ module.exports = conexion;
 /**
  * dentro del metodo utilizamos una funcion para comprobar si existe o no error para la conexion a la base de datos.
  * Con un condicional comprobamos si existe un error, que nos arroje los resultados de el error, si no un mensaje positivo.
- 
+*/
 conexion.connect(function(err){
     if(err){
         throw err;
     }else{
         console.log("conexion exitosa");
     }
-}); */
+}); 
 
 /**
  * para finalizar la ejecucion de la comprobacion hacemos lo siguiente:
