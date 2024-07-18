572d0055b1c68c12f8b795495438f98baa6fd6c9
Gabriel <jehernandezh@gcch.com>
2024-07-18 12:30:13 -0600

    php de prueba para heroku 2

--- cliente/index.php
+++ cliente/index.php
@@ -1,1 +1,11 @@
-<?php include_once("login.html"); ?>
+<?php include_once("agregarPasos.html"); 
+    include_once("agregarTienda.html");
+    include_once("agregarUsuarios.html");  
+    include_once("index.html"); 
+    include_once("listaAperturasUsuarios.html");
+    include_once("login.html");  
+    include_once("Pasos.html"); 
+    include_once("pasosNavbar.html"); 
+    include_once("pasosUsuarios.html"); 
+    include_once("usuarios.html"); 
+?>
